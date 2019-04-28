f99effeb602b22375f9aa03970f9829ace44ba4b
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-04-28 10:56:47 -0300

    Currenty Location

---
+++ src/components/Map/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default class Map extends Component {
+
+    state = {
+        region: null,
+    }
+
+    async componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            ({ coords: { latitude, longitude } }) => {
+                this.setState({
+                    region: {
+                        latitude,
+                        longitude,
+                        latitudeDelta: 0.0143,
+                        longitudeDelta: 0.0134,
+                    }
+                })
+            }, // sucesso
+            () => { }, // erro
+            {
+                timeout: 2000, // fica 2 segundo buscando a posição do usuario, caso der erro, cai no callback  erro
+                enableHighAccuracy: true, // pegar loc via gps
+                maximumAge: 1000, // guarda a localização de 1 em 1 segundo
+            }
+        )
+    }
+
+    render() {
+        const { region } = this.state;
+        return (
+            <View style={{ flex: 1 }}>
+                <MapView
+                    style={{ flex: 1 }}
+                    region={region}
+                    showsUserLocation
+                    loadingEnabled
+                />
+            </View>
+        )
+    }
+}
